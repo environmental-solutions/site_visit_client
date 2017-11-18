9cf51bf3248d374aa552d3a138e226e9bb170df5
Daniel Dosen <dgdosen@gmail.com>
2017-11-18 06:47:04 -0800

    wip: breaking out views to better marshall graphql data

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import ApolloClient from 'apollo-client-preset';
 import { ApolloProvider } from 'react-apollo';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import ProjectView from './views/ProjectView'
 
 const client = new ApolloClient({
   link: new HttpLink({
@@ -29,18 +31,20 @@ const ProjectQuery = gql`
 
 const ProjectsList = graphql(ProjectQuery)(props => <div><ul>{JSON.stringify(props.data.projects)}</ul></div>);
 
-class App extends Component {
 
+class App extends Component {
   render() {
     console.log("getting data");
     console.log(client.data);
     console.log("done getting data");
     return (
       <ApolloProvider client={client}>
-        <div>
-          <h1>This is Apollo</h1>
-          <ProjectsList />
-        </div>
+        <BrowserRouter>
+          <div>
+            <h1>This is Apollo</h1>
+            <Route exact path="/" component={ProjectView} />
+          </div>
+        </BrowserRouter>
       </ApolloProvider>
     );
   }
